49f609f5eb1e86a57a8721a81d57a741e137feff
Ben Chou <ben7152000@gmail.com>
2021-07-26 01:14:56 +0800

    fix vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/todolist-vue/'
-    : '/',
-  devServer: {
-    open: true,
-    proxy: {
-      '/': {
-        target: 'https://cryptic-caverns-48253.herokuapp.com',
-        pathRewrite: {
-          '^/': ''
-        }
-      }
-    }
-    // proxy: 'https://cryptic-caverns-48253.herokuapp.com' || 'http://localhost:8081'
-  }
+    : '/'
+  // devServer: {
+  //   open: true,
+  //   proxy: {
+  //     '/': {
+  //       target: 'https://cryptic-caverns-48253.herokuapp.com',
+  //       pathRewrite: {
+  //         '^/': ''
+  //       }
+  //     }
+  //   }
+  // proxy: 'https://cryptic-caverns-48253.herokuapp.com' || 'http://localhost:8081'
+  // }
 }
